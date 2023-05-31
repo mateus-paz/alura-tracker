e3f5b855a4855e81440902229ff3b61659528b18
Mateus Paz <mateuspaz.mp@gmail.com>
2023-05-31 15:17:56 -0300

    Compondo Componentes

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -10,30 +10,11 @@
           type="text"
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <TemporizadorTarefa @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -41,31 +22,24 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import TemporizadorTarefa from "./TemporizadorTarefa.vue";
 
 export default defineComponent({
   name: "FormularioTarefa",
-  data () {
+  components: {
+    TemporizadorTarefa
+  },
+  data(){
     return {
-      tempoEmSegundos: 0,
-      cronometro: 0
+      descricao: ''
     }
   },
-  computed: {
-    tempoDecorrido () : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11,19)
+  methods:{
+    finalizarTarefa (tempoDecorrido: number) : void {
+      console.log('tempo da tarefa', tempoDecorrido)
+      console.log('descricao da tarefa', this.descricao)
+      this.descricao = ''
     }
-  },
-  methods: {
-    iniciar() {
-      // começar a contagem
-      // 1 seg = 1000ms
-      this.cronometro = setInterval(()=>{
-        this.tempoEmSegundos += 1
-      },1000)
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    },
-  },
+  }
 });
 </script>
---
+++ src/components/TemporizadorTarefa.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <CronometroTarefa :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CronometroTarefa from './CronometroTarefa.vue';
+
+export default defineComponent({
+  name:"TemporizadorTarefa",
+  emits: ['aoTemporizadorFinalizado'],
+  components:{
+    CronometroTarefa
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometroRodando: false,
+    };
+  },
+  methods: {
+    iniciar() {
+      // começar a contagem
+      // 1 seg = 1000ms
+      this.cronometroRodando = true
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      this.cronometroRodando = false
+      clearInterval(this.cronometro)
+      this.$emit('aoTemporizadorFinalizado',this.tempoEmSegundos)
+      this.tempoEmSegundos = 0
+    },
+  },
+})
+
+</script>
